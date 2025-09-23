4a5e78a4b87344a19c1c7600b5437152d9c2c4e8
miora2621 <mioraprincia391@gmail.com>
2025-09-23 20:20:53 +0300

    templates for the application

---
+++ src/main/java/stage/bici/DBBridge/Controller/PageController.java
@@ -0,0 +1,13 @@
+package stage.bici.DBBridge.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+   @GetMapping("/")
+    public String loginForm() {
+        return "pages/Accueil";
+    }
+}
